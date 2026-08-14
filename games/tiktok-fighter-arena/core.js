4ff8a8ebd0331335dab839ffc815c5a79b6362a8
brilloburundi-ship-it <brilloburundi@gmail.com>
2026-08-15 00:14:34 +0200

    Lock Fighter Arena active roster to the agreed 18 NPCs

--- games/tiktok-fighter-arena/core.js
+++ games/tiktok-fighter-arena/core.js
@@ -1,1 +1,4 @@
 export * from './core-r18.js?v=r18-step1';
+import{S}from'./core-r18.js?v=r18-step1';
+const ACTIVE_R18=new Set(['street_mon','hero_knight','evil_wizard','huntress','martial_hero','medieval_king','martial_champion','evil_wizard_2','samurai','hero_knight_prime','fantasy_warrior','huntress_2','samurai_ronin','samurai_archer','samurai_commander','fire_wizard','lightning_mage','wanderer_magician']);
+export const cfg=id=>ACTIVE_R18.has(id)?S.manifest?.fighters?.[id]:undefined;
