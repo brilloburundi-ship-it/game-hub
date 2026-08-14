432f2ff756c422296276e09b02eb0ca0713d13f8
brilloburundi-ship-it <brilloburundi@gmail.com>
2026-08-15 01:41:12 +0200

    Bust Fire Wizard roster cache

--- games/tiktok-fighter-arena/asset-new-roster.js
+++ games/tiktok-fighter-arena/asset-new-roster.js
@@ -1,1 +1,1 @@
-export {R18_ROSTER_ASSETS as NEW_ROSTER_ASSETS} from './asset-r18-roster.js?v=r20-ios-fix2';
+export {R18_ROSTER_ASSETS as NEW_ROSTER_ASSETS} from './asset-r18-roster.js?v=r20-ios-fix3';
