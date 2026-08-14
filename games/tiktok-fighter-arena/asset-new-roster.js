58209d8a9992d753b6db4ba35e174ee773fc4815
brilloburundi-ship-it <brilloburundi@gmail.com>
2026-08-15 01:28:51 +0200

    Bust Fighter Arena R20 roster loader cache

--- games/tiktok-fighter-arena/asset-new-roster.js
+++ games/tiktok-fighter-arena/asset-new-roster.js
@@ -1,1 +1,1 @@
-export {R18_ROSTER_ASSETS as NEW_ROSTER_ASSETS} from './asset-r18-roster.js?v=r18-step1';
+export {R18_ROSTER_ASSETS as NEW_ROSTER_ASSETS} from './asset-r18-roster.js?v=r20-ios-fix1';
