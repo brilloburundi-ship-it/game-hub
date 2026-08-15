e4cce7c9a0ef371a008d86f4fc0218e0d0b2969e
brilloburundi-ship-it <brilloburundi@gmail.com>
2026-08-15 22:08:50 +0200

    Assemble wheeze victory taunt audio

---
+++ games/tiktok-fighter-arena/assets/audio/victory-taunt-wheeze-data.js
@@ -0,0 +1,6 @@
+// Fighter Arena victory taunt SFX supplied by the project owner; mono 32 kbps.
+import a from './victory-taunt-wheeze-0.js?v=1.0.0';
+import b from './victory-taunt-wheeze-1.js?v=1.0.0';
+import c from './victory-taunt-wheeze-2.js?v=1.0.0';
+import d from './victory-taunt-wheeze-3.js?v=1.0.0';
+export default `data:audio/mpeg;base64,${a}${b}${c}${d}`;
